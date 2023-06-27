b216cc49049e1b68ccb093038d25435d1d81280f
mohamed-abdelahi-haibelty <96340736+mohamed-abdelahi-haibelty@users.noreply.github.com>
2023-06-27 16:32:42 +0100

    update sidebar component

--- ar-anime-v1/src/components/Sidebar/Sidebar.tsx
+++ ar-anime-v1/src/components/Sidebar/Sidebar.tsx
@@ -8,10 +8,10 @@ type Props = {
 
 const Sidebar = (props: Props) => {
   return (
-    <header className="w-full md:w-[calc(100%-48px)] lg:w-24 h-14 lg:h-[calc(100%-64px)] p-4 md:p-6 bg-semiDarkBlue flex lg:flex-col justify-between items-center md:rounded-[10px] lg:rounded-[20px] fixed z-10">
+    <header className="w-full md:w-[calc(100%-48px)] lg:w-24 h-14 md:h-[72px] lg:h-[calc(100%-64px)] p-4 md:p-6 bg-semiDarkBlue flex lg:flex-col justify-between items-center md:rounded-[10px] lg:rounded-[20px] fixed top-0 md:top-8 z-10">
         <img src={movieImg} alt="movie img" />
-        <PiTelevisionBold className={'w-4 h-4 md:w-5 md:h-5 text-white cursor-pointer'}/>
-        <BsBookmarkFill className={'w-4 h-4 md:w-5 md:h-5 text-grBlue cursor-pointer'}/>
+        <PiTelevisionBold className={'w-4 h-4 md:w-5 md:h-5 text-white cursor-pointer hover:text-anRed transition'}/>
+        <BsBookmarkFill className={'w-4 h-4 md:w-5 md:h-5 text-grBlue cursor-pointer hover:text-anRed transition'}/>
     </header>
   )
 }
