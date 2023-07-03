e6c01adb4eb556865e4c7c028d95fcc27d737932
mohamed-abdelahi-haibelty <96340736+mohamed-abdelahi-haibelty@users.noreply.github.com>
2023-07-03 19:21:53 +0100

    update search component

--- ar-anime-v1/src/components/Search/Search.tsx
+++ ar-anime-v1/src/components/Search/Search.tsx
@@ -1,16 +1,18 @@
 import {RiSearchLine} from 'react-icons/ri'
 
 type Props = {
-    //
+  querry : string,
+  handleSearch : (e : string) => void
+  placeholder? : string
 }
 
-const Search = (props: Props) => {
+const Search = ({querry, handleSearch, placeholder}: Props) => {
   return (
     <div className='flex items-center gap-4 mt-[66px] md:mt-[128px] lg:mt-16'>
         <RiSearchLine className='text-white w-6 h-6 md:w-8 md:h-8'/>
         <div className='relative flex-1 group'>
-          <input className='w-full  text-white text-base md:text-2xl font-light 
-          placeholder-white/50 bg-transparent outline-none ' type="text" placeholder='Search for movies or TV series' />
+          <input value={querry} onChange={(e) => handleSearch(e.target.value)} className='w-full  text-white text-base md:text-2xl font-light 
+          placeholder-white/50 bg-transparent outline-none ' type="text" placeholder={placeholder} />
           <span className='w-full h-[1px] bg-grBlue hidden group-hover:block absolute left-0 bottom-[-5px] transition'></span>
         </div>
     </div>
